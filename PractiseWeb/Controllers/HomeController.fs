08f03f72c0ef94cdc9613f031841256d35f11b79
dean <deangibson03@gmail.com>
2024-03-18 09:06:20 +0200

    Add project files.

---
+++ PractiseWeb/Controllers/HomeController.fs
@@ -0,0 +1,36 @@
+﻿namespace PractiseWeb.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open System.Diagnostics
+
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+
+open PractiseWeb.Models
+
+type HomeController (logger : ILogger<HomeController>) =
+    inherit Controller()
+
+    member this.Index () =
+        this.View()
+
+    member this.About () =
+        this.View()
+
+    member this.Contact () =
+        this.View()
+
+    member this.MyWork () =
+        this.View()
+    [<ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)>]
+    member this.Error () =
+        let reqId = 
+            if isNull Activity.Current then
+                this.HttpContext.TraceIdentifier
+            else
+                Activity.Current.Id
+
+        this.View({ RequestId = reqId })
