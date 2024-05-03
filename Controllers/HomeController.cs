ebc9c4bde64def0f5d28ea6e01b5b28c2c96f9cf
Dean Gibson <deangibson03@gmail.com>
2024-05-03 16:56:23 +0200

    Updated the whole code base to C#, as it was mistakenly in F# before.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,15 +13,21 @@ namespace POE_CLVD.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Index() { return View(); }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult About() { return View(); }
+
+        public IActionResult Contact() { return View(); }
+
+        public IActionResult MyWork() { return View(); }
+
+        public IActionResult product1() { return View(); }
+
+        public IActionResult product2() { return View(); }
+
+        public IActionResult product3() { return View(); }
+
+        public IActionResult Privacy() { return View(); }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
