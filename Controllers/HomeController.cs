b917395cc3efb31b35c63d65a6c97a9492d491ef
Dean Gibson <deangibson03@gmail.com>
2024-05-14 16:00:50 +0200

    MyWork page working now

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using KhumaloCraft.Models;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace KhumaloCraft.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly Context _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, Context context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index() { return View(); }
@@ -19,7 +23,11 @@ namespace KhumaloCraft.Controllers
 
         public IActionResult Contact() { return View(); }
 
-        public IActionResult MyWork() { return View(); }
+        public IActionResult MyWork()
+        {
+            var products = _context.products.ToList(); // Fetch all products from the database
+            return View(products); // Pass the list of products to the view
+        }
 
         public IActionResult product1() { return View(); }
 
